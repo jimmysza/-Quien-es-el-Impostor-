278359050aa9a0d9c7137e5858d43eb131456e16
Jaime Perez <137426675+jimmysza@users.noreply.github.com>
2025-10-18 15:43:35 -0500

    Update palabras array with new words and clues
    Replaced the original words and clues with a new set that includes various categories such as movies, animals, brands, and Colombian culture.

--- script.js
+++ script.js
@@ -1,14 +1,463 @@
-const palabras = [
-    { palabra: "Mora", pista: "Fruta" },
-    { palabra: "Lápiz", pista: "Útil escolar" },
-    { palabra: "Tito Calderon", pista: "Cantante" },
-    { palabra: "Guitarra", pista: "Instrumento musical" },
-    { palabra: "Montaña", pista: "Formación geográfica" },
-    { palabra: "Cielo", pista: "Lo que ves arriba" },
-    { palabra: "Tijeras", pista: "Herramienta de corte" },
-    { palabra: "Nube", pista: "Flota en el cielo" },
-    { palabra: "Libro", pista: "Contiene historias" }
-];
+    const palabras = [
+
+
+        // 🎬 Personajes de películas
+        { palabra: "Harry Potter", pista: "Joven " },
+        { palabra: "Iron Man", pista: "Superhéroe" },
+        { palabra: "Frodo", pista: " Anillo " },
+        { palabra: "Buzz Lightyear", pista: "Juguete " },
+        { palabra: "Jack Sparrow", pista: " pirata " },
+
+
+        { palabra: "Cepillo", pista: "cabello" },
+        { palabra: "Billetera", pista: "Guardas " },
+        { palabra: "Mochila", pista: "Llevas" },
+
+        // 🐾 Animales
+        { palabra: "Jaguar", pista: "Felino" },
+        { palabra: "Delfín", pista: "Mamífero" },
+        { palabra: "Oso", pista: "peludo" },
+
+
+        { palabra: "Neymar", pista: " brasileñ@ " },
+        { palabra: "Falcao", pista: "legenda, colombian@" },
+
+        { palabra: "menu y bar", pista: "comida rapida" },
+        { palabra: "alex burger", pista: "hamburguesas" },
+        { palabra: "Walter White", pista: "Profesor " },
+        { palabra: "Tony Stark", pista: "Genio millonario" },
+        { palabra: "elon musk", pista: "Genio millonario" },
+        { palabra: "Sherlock Holmes", pista: "Detective " },
+        { palabra: "Hermione Granger", pista: "Bruja " },
+        { palabra: "Darth Vader", pista: "Señor oscuro" },
+        { palabra: "Elsa", pista: "Reina" },
+        { palabra: "Spider-Man", pista: "Héroe " },
+
+        { palabra: "Cristiano Ronaldo", pista: "Jugador de fútbol" },
+        { palabra: "Messi", pista: "argentino" },
+        { palabra: "duki", pista: "argentino" },
+        { palabra: "paulo londra", pista: "argentino" },
+        { palabra: "Shakira", pista: "Cantante, colombia " },
+        { palabra: "Bad Bunny", pista: "puertorriqueño, cantante" },
+        { palabra: "Karol G", pista: "cantante, colombia" },
+        { palabra: "J Balvin", pista: "Medellín, pegado" },
+        { palabra: "Feid", pista: "reguetón, conocido" },
+        { palabra: "James Rodríguez", pista: "crack, futbol" },
+        { palabra: "luisito comunica", pista: "crack, mexico" },
+
+        // Marcas de lujo
+        { palabra: "Gucci", pista: "lujosa" },
+        { palabra: "Rolex", pista: "estatus" },
+        { palabra: "Ferrari", pista: "rojo" },
+        { palabra: "Chanel", pista: "fragancia" },
+        { palabra: "Louis Vuitton", pista: "de todo" },
+        { palabra: "Prada", pista: "Gafas negras" },
+
+        // Colombia
+        { palabra: "Arepas", pista: "maíz" },
+        { palabra: "Café", pista: "Grano" },
+        { palabra: "arroz", pista: "Grano" },
+        { palabra: "Vallenato", pista: "Acordeón " },
+        { palabra: "Sombrero Vueltiao", pista: "Tejido " },
+
+        // Objetos
+        { palabra: "Celular", pista: "Pantalla" },
+        { palabra: "Llaves", pista: "Abrir" },
+        { palabra: "Cepillo", pista: "suaves" },
+        { palabra: "Paraguas", pista: "Techo " },
+        { palabra: "Reloj", pista: "tiempo" },
+
+        // Animales
+        { palabra: "Perezoso", pista: "lento" },
+        { palabra: "Cóndor", pista: "Ave" },
+        { palabra: "Capibara", pista: "tiern@" },
+        { palabra: "Tucán", pista: "Pico " },
+        { palabra: "Colibrí", pista: "Ave" },
+
+        // Jugadores
+        { palabra: "Falcao", pista: "Tigre cafetero" },
+        { palabra: "Linda Caicedo", pista: "Estrella femenina" },
+        { palabra: "Mbappé", pista: "Velocista francés" },
+        // Personajes / Famosos
+{ palabra: "Mickey Mouse", pista: "personaje" },
+{ palabra: "Marilyn Monroe", pista: "famosa" },
+{ palabra: "Michael Jordan", pista: "jugador" },
+{ palabra: "Taylor Swift", pista: "cantante" },
+{ palabra: "Drake", pista: "rapero" },
+{ palabra: "Selena", pista: "cantante" },
+{ palabra: "Rafael Nadal", pista: "tenista" },
+{ palabra: "Lionel Messi", pista: "argentino" },
+{ palabra: "Rihanna", pista: "cantante" },
+{ palabra: "The Rock", pista: "actor" },
+
+// Alimentos
+{ palabra: "Pizza", pista: "alimento" },
+{ palabra: "Hamburguesa", pista: "comida" },
+{ palabra: "Banano", pista: "fruta" },
+{ palabra: "Arroz", pista: "grano" },
+{ palabra: "Leche", pista: "líquido" },
+{ palabra: "Pan", pista: "básico" },
+{ palabra: "Huevo", pista: "desayuno" },
+{ palabra: "Pollo", pista: "carne" },
+
+// Marcas / Objetos
+{ palabra: "Nike", pista: "deporte" },
+{ palabra: "Apple", pista: "tecnología" },
+{ palabra: "Coca-Cola", pista: "refresco" },
+{ palabra: "Adidas", pista: "ropa" },
+{ palabra: "Samsung", pista: "móvil" },
+{ palabra: "Zara", pista: "ropa" },
+{ palabra: "Netflix", pista: "streaming" },
+{ palabra: "Instagram", pista: "red" },
+
+// Animales
+{ palabra: "León", pista: "rey" },
+{ palabra: "Tiburón", pista: "mar" },
+{ palabra: "Águila", pista: "cielo" },
+{ palabra: "Serpiente", pista: "reptil" },
+{ palabra: "Gato", pista: "mascota" },
+{ palabra: "Perro", pista: "fiel" },
+{ palabra: "Elefante", pista: "grande" },
+{ palabra: "Ballena", pista: "oceano" },
+
+// Lugares / Conceptos
+{ palabra: "Luna", pista: "espacio" },
+{ palabra: "Sol", pista: "fuego" },
+{ palabra: "Hielo", pista: "frío" },
+{ palabra: "Volcán", pista: "fuego" },
+{ palabra: "Desierto", pista: "calor" },
+{ palabra: "Montaña", pista: "alto" },
+{ palabra: "Río", pista: "agua" },
+{ palabra: "Playa", pista: "arena" },
+
+// Colombia / LATAM (genérico)
+{ palabra: "Bandeja Paisa", pista: "alimento" },
+{ palabra: "Salsa", pista: "baile" },
+{ palabra: "Reguetón", pista: "música" },
+{ palabra: "Bogotá", pista: "capital" },
+{ palabra: "Caribe", pista: "mar" },
+{ palabra: "Andes", pista: "montañas" },
+{ palabra: "Plátano", pista: "fruta" },
+{ palabra: "Ajiaco", pista: "sopa" },
+
+// Jugadores / Deportistas
+{ palabra: "Neymar", pista: "brasileño" },
+{ palabra: "Kylian Mbappé", pista: "francés" },
+{ palabra: "Alex Morgan", pista: "futbolista" },
+{ palabra: "Simone Biles", pista: "gimnasta" },
+{ palabra: "Usain Bolt", pista: "velocista" },
+{ palabra: "Pele", pista: "leyenda" },
+{ palabra: "Diego Maradona", pista: "ídolo" },
+{ palabra: "Marta", pista: "fútbol femenino" },
+// Personajes / Famosos
+{ palabra: "Mickey Mouse", pista: "personaje" },
+{ palabra: "Marilyn Monroe", pista: "famosa" },
+{ palabra: "Michael Jordan", pista: "jugador" },
+{ palabra: "Taylor Swift", pista: "cantante" },
+{ palabra: "Drake", pista: "rapero" },
+{ palabra: "Selena", pista: "cantante" },
+{ palabra: "Rafael Nadal", pista: "tenista" },
+{ palabra: "Lionel Messi", pista: "argentino" },
+{ palabra: "Rihanna", pista: "cantante" },
+{ palabra: "The Rock", pista: "actor" },
+
+// Alimentos
+{ palabra: "Pizza", pista: "alimento" },
+{ palabra: "Hamburguesa", pista: "comida" },
+{ palabra: "Banano", pista: "fruta" },
+{ palabra: "Arroz", pista: "grano" },
+{ palabra: "Leche", pista: "líquido" },
+{ palabra: "Pan", pista: "básico" },
+{ palabra: "Huevo", pista: "desayuno" },
+{ palabra: "Pollo", pista: "carne" },
+
+// Marcas / Objetos
+{ palabra: "Nike", pista: "deporte" },
+{ palabra: "Apple", pista: "tecnología" },
+{ palabra: "Coca-Cola", pista: "refresco" },
+{ palabra: "Adidas", pista: "ropa" },
+{ palabra: "Samsung", pista: "móvil" },
+{ palabra: "Zara", pista: "ropa" },
+{ palabra: "Netflix", pista: "streaming" },
+{ palabra: "Instagram", pista: "red" },
+
+// Animales
+{ palabra: "León", pista: "rey" },
+{ palabra: "Tiburón", pista: "mar" },
+{ palabra: "Águila", pista: "cielo" },
+{ palabra: "Serpiente", pista: "reptil" },
+{ palabra: "Gato", pista: "mascota" },
+{ palabra: "Perro", pista: "fiel" },
+{ palabra: "Elefante", pista: "grande" },
+{ palabra: "Ballena", pista: "oceano" },
+
+// Lugares / Conceptos
+{ palabra: "Luna", pista: "espacio" },
+{ palabra: "Sol", pista: "fuego" },
+{ palabra: "Hielo", pista: "frío" },
+{ palabra: "Volcán", pista: "fuego" },
+{ palabra: "Desierto", pista: "calor" },
+{ palabra: "Montaña", pista: "alto" },
+{ palabra: "Río", pista: "agua" },
+{ palabra: "Playa", pista: "arena" },
+
+// Colombia / LATAM (genérico)
+{ palabra: "Bandeja Paisa", pista: "alimento" },
+{ palabra: "Salsa", pista: "baile" },
+{ palabra: "Reguetón", pista: "música" },
+{ palabra: "Bogotá", pista: "capital" },
+{ palabra: "Caribe", pista: "mar" },
+{ palabra: "Andes", pista: "montañas" },
+{ palabra: "Plátano", pista: "fruta" },
+{ palabra: "Ajiaco", pista: "sopa" },
+
+// Jugadores / Deportistas
+{ palabra: "Neymar", pista: "brasileño" },
+{ palabra: "Kylian Mbappé", pista: "francés" },
+{ palabra: "Alex Morgan", pista: "futbolista" },
+{ palabra: "Simone Biles", pista: "gimnasta" },
+{ palabra: "Usain Bolt", pista: "velocista" },
+{ palabra: "Pele", pista: "leyenda" },
+{ palabra: "Diego Maradona", pista: "ídolo" },
+{ palabra: "Marta", pista: "fútbol femenino" },
+// Caricaturas clásicas y modernas
+{ palabra: "Bugs Bunny", pista: "conejo" },
+{ palabra: "Pato Lucas", pista: "cazador" },
+{ palabra: "Los Simpson", pista: "familia" },
+{ palabra: "Bob Esponja", pista: "esponja" },
+{ palabra: "Picachu", pista: "eléctrico" },
+{ palabra: "Dragon Ball", pista: "esferas" },
+{ palabra: "Los Picapiedra", pista: "prehistoria" },
+{ palabra: "Tom y Jerry", pista: "gato y ratón" },
+{ palabra: "Scooby-Doo", pista: "misterio" },
+{ palabra: "Ben 10", pista: "reloj" },
+{ palabra: "Power Rangers", pista: "equipo" },
+{ palabra: "Los Rugrats", pista: "bebés" },
+{ palabra: "Dora la Exploradora", pista: "mapa" },
+{ palabra: "Phineas y Ferb", pista: "inventores" },
+{ palabra: "Los Supersónicos", pista: "futuro" },
+{ palabra: "Avatar", pista: "elementos" },
+{ palabra: "Teen Titans", pista: "jóvenes" },
+{ palabra: "Rick y Morty", pista: "ciencia" },
+{ palabra: "Los Minions", pista: "amarillos" },
+{ palabra: "Garfield", pista: "gato" },
+// Cantantes
+{ palabra: "Diomedes Díaz", pista: "vallenato" },
+{ palabra: "Silvestre Dangond", pista: "cantante" },
+{ palabra: "Luis Fonsi", pista: "puertorriqueño" },
+{ palabra: "Carlos Vives", pista: "colombiano" },
+{ palabra: "Farid Dieck", pista: "cantante" },
+
+// Tus amigos (pista genérica: "amigo" o "conocido")
+{ palabra: "Jaime", pista: "amigo" },
+{ palabra: "Luis", pista: "amigo" },
+{ palabra: "Juanda", pista: "amigo" },
+{ palabra: "María Lucía", pista: "amiga" },
+{ palabra: "María Ale", pista: "amiga" },
+{ palabra: "Sofía", pista: "amiga" },
+
+// Personajes de miedo
+{ palabra: "Pennywise", pista: "payaso" },
+{ palabra: "Freddy Krueger", pista: "guante" },
+{ palabra: "Michael Myers", pista: "máscara" },
+{ palabra: "Chucky", pista: "muñeco" },
+{ palabra: "La Llorona", pista: "fantasma" },
+{ palabra: "Slenderman", pista: "siniestro" },
+
+// Influencers, marcas y figuras populares
+{ palabra: "CBUM", pista: "físico" },
+{ palabra: "Duolingo", pista: "app" },
+{ palabra: "Westcol", pista: "marca" },
+{ palabra: "Iohine", pista: "youtuber" },
+{ palabra: "Samsung", pista: "teléfono" },
+{ palabra: "Nokia", pista: "móvil" },
+{ palabra: "Blackberry", pista: "teclado" },
+{ palabra: "MrBeast", pista: "youtuber" },
+{ palabra: "Luisito Comunica", pista: "youtuber" },
+// Barrios de Cartagena
+{ palabra: "Bocagrande", pista: "barrio" },
+{ palabra: "Crespo", pista: "barrio" },
+{ palabra: "Pie de la Popa", pista: "barrio" },
+{ palabra: "La Manga", pista: "barrio" },
+{ palabra: "El Pozón", pista: "barrio" },
+
+// Comida típica de Cartagena / Caribe
+{ palabra: "Arepa de huevo", pista: "comida" },
+{ palabra: "Cazuela de mariscos", pista: "sopa" },
+{ palabra: "Patacón", pista: "frito" },
+{ palabra: "Sancocho", pista: "guiso" },
+{ palabra: "Arroz con coco", pista: "plato" },
+
+// Marcas / Productos comunes (usados localmente)
+{ palabra: "Pan", pista: "alimento" },
+{ palabra: "Coca-Cola", pista: "refresco" },
+{ palabra: "Cage", pista: "gaseosa" },
+
+// Restaurantes o referencias conocidas en Cartagena
+{ palabra: "La Cevichería", pista: "restaurante" },
+{ palabra: "Don Juan", pista: "restaurante" },
+{ palabra: "El Bistro", pista: "comida" },
+{ palabra: "Crepes & Waffles", pista: "postre" },
+{ palabra: "La Mulata", pista: "típico" },
+
+// 🇨🇴 Cartagena & Colombia
+{ palabra: "Getsemaní", pista: "barrio" },
+{ palabra: "Castillo San Felipe", pista: "fortaleza" },
+{ palabra: "Islas del Rosario", pista: "playa" },
+{ palabra: "Plaza Bolívar", pista: "lugar" },
+{ palabra: "Ceviche", pista: "comida" },
+{ palabra: "Bollo", pista: "maíz" },
+{ palabra: "Champeta", pista: "música" },
+{ palabra: "Palenque", pista: "cultura" },
+{ palabra: "Ciénaga", pista: "pueblo" },
+{ palabra: "Turbaco", pista: "municipio" },
+
+// 🎤 Cantantes (Colombia + LATAM, menos mainstream)
+{ palabra: "Lisandro Meza", pista: "cumbia" },
+{ palabra: "Nelson Díaz", pista: "vallenato" },
+{ palabra: "Kaleth Morales", pista: "vallenato" },
+{ palabra: "Guayacán", pista: "salsa" },
+{ palabra: "Joe Arroyo", pista: "salsa" },
+{ palabra: "Totó la Momposina", pista: "folclor" },
+{ palabra: "Petrona Martínez", pista: "bullerengue" },
+{ palabra: "Herencia de Timbiquí", pista: "champeta" },
+{ palabra: "Systema Solar", pista: "electrónica" },
+{ palabra: "Margarita Siempre Viva", pista: "rock" },
+
+// 📱 Youtubers / Influencers / Figuras digitales
+{ palabra: "Juanpa Zurita", pista: "youtuber" },
+{ palabra: "AuronPlay", pista: "youtuber" },
+{ palabra: "El Bananero", pista: "comediante" },
+{ palabra: "Dross", pista: "terror" },
+{ palabra: "YosStop", pista: "influencer" },
+{ palabra: "Luzu", pista: "streamer" },
+{ palabra: "TheDonato", pista: "youtuber" },
+{ palabra: "Fernanfloo", pista: "gamer" },
+{ palabra: "Ami Rodríguez", pista: "belleza" },
+{ palabra: "Duki", pista: "rapero" },
+
+// 👥 Tus amigos (nombres reales → pista genérica)
+{ palabra: "Valeria", pista: "amiga" },
+{ palabra: "Camilo", pista: "amigo" },
+{ palabra: "Daniela", pista: "amiga" },
+{ palabra: "Andrés", pista: "amigo" },
+{ palabra: "Lucía", pista: "amiga" },
+{ palabra: "Mateo", pista: "amigo" },
+{ palabra: "Isabella", pista: "amiga" },
+{ palabra: "Sebastián", pista: "amigo" },
+{ palabra: "Paula", pista: "amiga" },
+{ palabra: "Felipe", pista: "amigo" },
+
+// 👻 Personajes de miedo / terror
+{ palabra: "Annabelle", pista: "muñeca" },
+{ palabra: "It", pista: "payaso" },
+{ palabra: "El Jinete Sin Cabeza", pista: "fantasma" },
+{ palabra: "El Hombre de la Bolsa", pista: "leyenda" },
+{ palabra: "La Patasola", pista: "mito" },
+{ palabra: "El Mohán", pista: "río" },
+{ palabra: "Bloody Mary", pista: "espejo" },
+{ palabra: "El Cucuy", pista: "niños" },
+{ palabra: "El Silbón", pista: "llanero" },
+{ palabra: "La Llorona", pista: "río" },
+
+// 📺 Caricaturas / Series animadas
+{ palabra: "Los Simpson", pista: "familia" },
+{ palabra: "Dragon Ball Z", pista: "guerreros" },
+{ palabra: "Naruto", pista: "ninja" },
+{ palabra: "One Piece", pista: "piratas" },
+{ palabra: "SpongeBob", pista: "esponja" },
+{ palabra: "Los Picapiedra", pista: "piedra" },
+{ palabra: "Ben 10", pista: "alien" },
+{ palabra: "Phineas y Ferb", pista: "verano" },
+{ palabra: "Avatar", pista: "elementos" },
+{ palabra: "Rick y Morty", pista: "ciencia" },
+
+// 📚 Libros famosos
+{ palabra: "Don Quijote", pista: "caballero" },
+{ palabra: "1984", pista: "vigilancia" },
+{ palabra: "El Principito", pista: "planeta" },
+{ palabra: "Cien Años de Soledad", pista: "Macondo" },
+{ palabra: "Crónica de una Muerte Anunciada", pista: "destino" },
+{ palabra: "Rayuela", pista: "lectura" },
+{ palabra: "La Odisea", pista: "viaje" },
+{ palabra: "Frankenstein", pista: "criatura" },
+{ palabra: "Drácula", pista: "vampiro" },
+{ palabra: "El Alquimista", pista: "sueño" },
+
+// 🧴 Objetos cotidianos
+{ palabra: "Cuchara", pista: "comer" },
+{ palabra: "Vaso", pista: "beber" },
+{ palabra: "Jabón", pista: "limpiar" },
+{ palabra: "Toalla", pista: "secar" },
+{ palabra: "Cargador", pista: "energía" },
+{ palabra: "Bolsa", pista: "llevar" },
+{ palabra: "Espejo", pista: "reflejo" },
+{ palabra: "Cepillo", pista: "dientes" },
+{ palabra: "Llaves", pista: "abrir" },
+{ palabra: "Reloj", pista: "tiempo" },
+
+// 🍽️ Comida típica (Colombia / Caribe)
+{ palabra: "Sancocho", pista: "guiso" },
+{ palabra: "Arepas", pista: "maíz" },
+{ palabra: "Patacón", pista: "plátano" },
+{ palabra: "Arroz con coco", pista: "caribe" },
+{ palabra: "Bollo", pista: "envuelto" },
+{ palabra: "Mote de queso", pista: "sopa" },
+{ palabra: "Carimañola", pista: "rellena" },
+{ palabra: "Empanada", pista: "frita" },
+{ palabra: "Chicharrón", pista: "crujiente" },
+{ palabra: "Tamales", pista: "hoja" },
+
+// 🏙️ Barrios / Lugares de Cartagena
+{ palabra: "El Cabrero", pista: "barrio" },
+{ palabra: "Olaya Herrera", pista: "barrio" },
+{ palabra: "San Diego", pista: "barrio" },
+{ palabra: "La Boquilla", pista: "playa" },
+{ palabra: "Tierra Bomba", pista: "isla" },
+{ palabra: "Bocagrande", pista: "hotel" },
+{ palabra: "Manga", pista: "bahía" },
+{ palabra: "Crespo", pista: "mercado" },
+{ palabra: "Pie de la Popa", pista: "mirador" },
+{ palabra: "El Pozón", pista: "comuna" },
+
+// 📱 Marcas (tecnología, bebidas, lujo)
+{ palabra: "Apple", pista: "móvil" },
+{ palabra: "Nike", pista: "deporte" },
+{ palabra: "Adidas", pista: "ropa" },
+{ palabra: "Coca-Cola", pista: "refresco" },
+{ palabra: "Cage", pista: "gaseosa" },
+{ palabra: "Postobón", pista: "bebida" },
+{ palabra: "Samsung", pista: "pantalla" },
+{ palabra: "Gucci", pista: "lujo" },
+{ palabra: "Rolex", pista: "reloj" },
+{ palabra: "Louis Vuitton", pista: "bolsos" },
+
+// ⚽ Deportistas famosos
+{ palabra: "James Rodríguez", pista: "colombiano" },
+{ palabra: "Radamel Falcao", pista: "delantero" },
+{ palabra: "Lionel Messi", pista: "argentino" },
+{ palabra: "Cristiano Ronaldo", pista: "portugués" },
+{ palabra: "Neymar", pista: "brasileño" },
+{ palabra: "Kylian Mbappé", pista: "francés" },
+{ palabra: "Linda Caicedo", pista: "futbolista" },
+{ palabra: "Marta", pista: "brasileña" },
+{ palabra: "Carlos Valderrama", pista: "peluca" },
+{ palabra: "Iván Córdoba", pista: "defensa" },
+
+// 🎬 Personajes de películas
+{ palabra: "Iron Man", pista: "armadura" },
+{ palabra: "Harry Potter", pista: "varita" },
+{ palabra: "Frodo", pista: "anillo" },
+{ palabra: "Jack Sparrow", pista: "pirata" },
+{ palabra: "Walter White", pista: "químico" },
+{ palabra: "Tony Stark", pista: "millonario" },
+{ palabra: "Sherlock Holmes", pista: "lupa" },
+{ palabra: "Darth Vader", pista: "respirar" },
+{ palabra: "Elsa", pista: "hielo" },
+{ palabra: "Spider-Man", pista: "telaraña" }
+
+    ];
+
 
 const container = document.querySelector(".container-cards");
 const btnEmpezar = document.getElementById("empezar");
